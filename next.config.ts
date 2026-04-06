9be8a59b9546521cfd2c709951ccab93942b8808
Tam-Pham <tamphd16@gmail.com>
2026-04-06 17:21:34 +0700

    feat: Configure Next.js for static export
    Set the output to 'export' and configure the distDir to 'dist' for static site generation. Also, disable image optimization for unoptimized images. This change prepares the application for deployment as a static site.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'dist',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
